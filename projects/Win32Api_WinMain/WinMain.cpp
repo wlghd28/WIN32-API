9828bbf2dcd641121a310865e92bde3be55f3865
jihong <hojindohong@nate.com>
2022-08-24 13:19:23 +0900

    upload

--- projects/Win32Api_WinMain/WinMain.cpp
+++ projects/Win32Api_WinMain/WinMain.cpp
@@ -4,8 +4,11 @@
     /SUBSYSTEM:CONSOLE => mainCRTStartup / wmainCRTStartup => main / wmain
     /SUBSYSTEM:WINDOWS => WinMainCRTStartup / wWinMainCRTStartup => WinMain / wWinMain
     /DLL => _DllMainCRTStartup => DllMain
+    
+    빈 프로젝트로 프로젝트를 생성 했을 때 WINAPI 함수를 쓸 때 헤더파일을 포함 시켰는데도 LNK2019 오류가 난다면
+    Visual Studio의 최신 표준 라이브러리 변경에 의해 추가해야 할 라이브러리들이 추가됐기 때문이다.
+    따라서 msvcrt.lib, ucrt.lib, vcruntime.lib 3개의 라이브러리를 추가 종속성에 추가 시켜줘야 한다.
 */
-
 
 //// WinMain.cpp
 //--------------------------------------------------------------------------------------
@@ -13,7 +16,21 @@
 //--------------------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, LPSTR CmdPrm, int CmdShow)
 {
-    MessageBox(NULL, _T("WinMain"), _T("메시지 박스"), MB_OK);
+    //MessageBox(NULL, _T("WinMain"), _T("메시지 박스"), MB_OK);
+    CHAR Buff[128];
+    HINSTANCE hDll1, hDll2;
+
+    hDll1 = LoadLibrary("msimg32.dll");
+    hDll2 = LoadLibrary("msimsg.dll");
+
+    Printf("hModule\t = %X\n", GetModuleHandle(NULL));
+    Printf("hInst\t = %X\n", hInst);
+    Printf("hDll1\t = %X\n", hDll1);
+    Printf("hDll2\t = %X\n", hDll2);
+
+    Printf(Buff);
+    Printf("Hellow");
+
     return 0;
 }
 
@@ -31,8 +48,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, LPSTR CmdPrm, int CmdSh
 //--------------------------------------------------------------------------------------
 int WinMainCRTStartup(void)
 {
-
-    MessageBox(NULL, _T("WinMainCRTStartup"), _T("메시지 박스"), MB_OK);
+    //MessageBox(NULL, _T("WinMainCRTStartup"), _T("메시지 박스"), MB_OK);
 
     int iRet;
     STARTUPINFO SI;
@@ -49,4 +65,27 @@ int WinMainCRTStartup(void)
     ExitProcess(iRet);
 
     return iRet;
+}
+
+
+
+//--------------------------------------------------------------------------------------
+//  스크린 디버그 함수
+//--------------------------------------------------------------------------------------
+void Printf(LPCSTR FormStr, ...)  // 스크린 디버그 함수 (가변인자)
+{
+    HDC hDC;
+    va_list VL;
+    CHAR Buff[MAX_PATH];
+    static int DebugPosY = 0;
+
+    va_start(VL, FormStr);
+    StringCbPrintf(Buff, sizeof(Buff), FormStr, VL);
+    va_end(VL);
+
+    hDC = GetDC(NULL);
+    TextOut(hDC, 0, DebugPosY, Buff, lstrlen(Buff));
+    if ((DebugPosY += 16) >= GetSystemMetrics(SM_CYSCREEN)) DebugPosY = 0;
+
+    //OutputDebugString(Buff);
 }
--- projects/Win32Api_WinMain/WinMain.h
+++ projects/Win32Api_WinMain/WinMain.h
@@ -1,8 +1,7 @@
 #pragma once
 
-
-
 //// WinMain.h
+
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 // Windows 헤더 파일
 #include <Windows.h>
@@ -11,3 +10,16 @@
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+#include <strsafe.h>    // wvnsprintf()
+
+
+// 함수선언
+int WINAPI WinMain
+(
+    HINSTANCE hInst, 
+    HINSTANCE hInstPrev, 
+    LPSTR CmdPrm, 
+    int CmdShow
+);
+
+void Printf(LPCSTR FormStr, ...);   // 스크린 디버그 함수 (가변인자)
